0bf68af646856464d9e804675a23bdb625391e03
Adam L Edwards <adam@aeweb.io>
2022-11-11 12:04:15 +0000

    chore: add volume to playwright node_modules

--- scripts/run-playwright-container.sh
+++ scripts/run-playwright-container.sh
@@ -4,4 +4,4 @@ set -e
 CONTAINER_RUNNER="${CONTAINER_RUNNER:-podman}"
 PLAYWRIGHT_VERSION=$(scripts/get-playwright-version.sh)
 
-$CONTAINER_RUNNER run --rm -v "$(pwd)":/work/ -w /work/ -it -e CI mcr.microsoft.com/playwright:"${PLAYWRIGHT_VERSION}" "$@"
+$CONTAINER_RUNNER run --rm -v "$(pwd)":/work/ -v /work/node_modules -w /work/ -it -e CI mcr.microsoft.com/playwright:"${PLAYWRIGHT_VERSION}" "$@"
