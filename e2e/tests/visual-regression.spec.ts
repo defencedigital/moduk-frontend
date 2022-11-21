6aedc2d7508e835436f7b6cee913918f73b49a52
jpveooys <66470099+jpveooys@users.noreply.github.com>
2022-11-21 10:27:24 +0000

    test(e2e): Move test files to subdirectory
    This moves all end-to-end test files to `e2e/tests` so that non-test files can be put in `e2e`.
    
    `e2e-output` was also moved to `e2e/output`.

--- e2e/tests/visual-regression.spec.ts
+++ e2e/tests/visual-regression.spec.ts
@@ -1,5 +1,5 @@
 import { expect, test } from '@playwright/test'
-import { findExamples } from '../src/test-utils'
+import { findExamples } from '../../src/test-utils'
 
 test.describe('@visual-regression', () => {
   findExamples().forEach(([component, exampleNames]) => {
