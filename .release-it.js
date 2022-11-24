d7b4a2d4183b0b99aa979865e804ec982fa4d8cd
jpveooys <66470099+jpveooys@users.noreply.github.com>
2022-11-24 16:54:47 +0000

    fixup! Correct release-it changelog config

--- .release-it.js
+++ .release-it.js
@@ -13,69 +13,71 @@ module.exports = {
   },
   plugins: {
     '@release-it/conventional-changelog': {
-      preset: 'conventionalcommits',
-      types: [
-        {
-          type: 'feat',
-          section: 'Features',
-        },
-        {
-          type: 'fix',
-          section: 'Bug fixes',
-        },
-        {
-          type: 'perf',
-          section: 'Performance',
-        },
-        {
-          type: 'revert',
-          section: 'Reversions',
-        },
-        {
-          type: 'deps',
-          section: 'Dependencies',
-        },
-        {
-          type: 'build',
-          section: 'Internal changes',
-        },
-        {
-          type: 'chore',
-          section: 'Internal changes',
-        },
-        {
-          type: 'deps-dev',
-          section: 'Internal changes',
-        },
-        {
-          type: 'style',
-          section: 'Internal changes',
-        },
-        {
-          type: 'refactor',
-          section: 'Internal changes',
-        },
-        {
-          type: 'docs',
-          section: 'Documentation',
-          hidden: 'true',
-        },
-        {
-          type: 'release',
-          section: 'Releases',
-          hidden: 'true',
-        },
-        {
-          type: 'test',
-          section: 'Testing',
-          hidden: 'true',
-        },
-        {
-          type: 'ci',
-          section: 'Continuous integration',
-          hidden: 'true',
-        },
-      ],
+      preset: {
+        name: 'conventionalcommits',
+        types: [
+          {
+            type: 'feat',
+            section: 'Features',
+          },
+          {
+            type: 'fix',
+            section: 'Bug fixes',
+          },
+          {
+            type: 'perf',
+            section: 'Performance',
+          },
+          {
+            type: 'revert',
+            section: 'Reversions',
+          },
+          {
+            type: 'deps',
+            section: 'Dependencies',
+          },
+          {
+            type: 'build',
+            section: 'Internal changes',
+          },
+          {
+            type: 'chore',
+            section: 'Internal changes',
+          },
+          {
+            type: 'deps-dev',
+            section: 'Internal changes',
+          },
+          {
+            type: 'style',
+            section: 'Internal changes',
+          },
+          {
+            type: 'refactor',
+            section: 'Internal changes',
+          },
+          {
+            type: 'docs',
+            section: 'Documentation',
+            hidden: 'true',
+          },
+          {
+            type: 'release',
+            section: 'Releases',
+            hidden: 'true',
+          },
+          {
+            type: 'test',
+            section: 'Testing',
+            hidden: 'true',
+          },
+          {
+            type: 'ci',
+            section: 'Continuous integration',
+            hidden: 'true',
+          },
+        ],
+      },
     },
   },
 }
