811b4d803448a46bf929e00bb8d56e630d8197f7
jpveooys <66470099+jpveooys@users.noreply.github.com>
2022-12-08 16:56:28 +0000

    fix(panel): Add support for call blocks
    This makes the panel component work correctly when a [call block](https://mozilla.github.io/nunjucks/templating.html#call) is used.

--- src/nunjucks/moduk/components/panel/__tests__/macro.test.ts
+++ src/nunjucks/moduk/components/panel/__tests__/macro.test.ts
@@ -1,9 +1,35 @@
 import { describe, expect, it } from 'vitest'
 
-import { renderFile } from '../../../../../test-utils'
+import { render, renderFile } from '../../../../../test-utils'
 
 describe('Panel', async () => {
-  it('renders the content', () => {
-    expect(() => renderFile('moduk/components/panel/__examples__/default.njk')).not.toThrowError()
+  it('renders the content using the html property', () => {
+    const element = renderFile('moduk/components/panel/__examples__/default.njk')
+    expect(element).toHaveTextContent('HDJ2123F')
+  })
+
+  it('renders the content when using the text property', () => {
+    const element = render(`
+{% from "moduk/components/panel/macro.njk" import modukPanel %}
+
+{{ modukPanel({
+  titleText: "Application complete",
+  text: "Your reference number is HDJ2123F"
+}) }}
+`)
+    expect(element).toHaveTextContent('HDJ2123F')
+  })
+
+  it('renders the content when using a call block', () => {
+    const element = render(`
+{% from "moduk/components/panel/macro.njk" import modukPanel %}
+
+{% call modukPanel({
+  titleText: "Application complete" 
+}) %}
+  Your reference number<br><strong>HDJ2123F</strong>
+{% endcall %}
+`)
+    expect(element).toContainHTML('Your reference number<br><strong>HDJ2123F</strong>')
   })
 })
