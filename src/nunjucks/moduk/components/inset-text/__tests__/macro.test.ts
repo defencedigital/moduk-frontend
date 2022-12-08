630dea44314272e697597b349d88da939b9773aa
jpveooys <66470099+jpveooys@users.noreply.github.com>
2022-12-08 16:55:33 +0000

    fix(inset-text): Add support for call blocks
    This makes the inset text component work correctly when a [call block](https://mozilla.github.io/nunjucks/templating.html#call) is used.

--- src/nunjucks/moduk/components/inset-text/__tests__/macro.test.ts
+++ src/nunjucks/moduk/components/inset-text/__tests__/macro.test.ts
@@ -1,9 +1,35 @@
 import { describe, expect, it } from 'vitest'
 
-import { renderFile } from '../../../../../test-utils'
+import { render, renderFile } from '../../../../../test-utils'
 
 describe('Inset text', async () => {
-  it('renders the content', () => {
-    expect(() => renderFile('moduk/components/inset-text/__examples__/default.njk')).not.toThrowError()
+  it('renders the content when using the text property', () => {
+    const element = renderFile('moduk/components/inset-text/__examples__/default.njk')
+    expect(element).toHaveTextContent('You’ll get confirmation')
+  })
+
+  it('renders the content when using the html property', () => {
+    const element = render(`
+{% from "moduk/components/inset-text/macro.njk" import modukInsetText %}
+
+{{ modukInsetText({
+  summaryText: "Help with organisation",
+  html: "You’ll get confirmation that we have received your report within 5 working days."
+}) }}
+`)
+    expect(element).toHaveTextContent('You’ll get confirmation')
+  })
+
+  it('renders the content when using a call block', () => {
+    const element = render(`
+{% from "moduk/components/inset-text/macro.njk" import modukInsetText %}
+
+{% call modukInsetText({
+  summaryText: "Help with organisation" 
+}) %}
+  You’ll get confirmation<br> that we have received your report within 5 working days.
+{% endcall %}
+`)
+    expect(element).toContainHTML('You’ll get confirmation<br> that')
   })
 })
