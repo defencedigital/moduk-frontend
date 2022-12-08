f8c5816389d4fe7d3fee736368b1b54f9403b807
jpveooys <66470099+jpveooys@users.noreply.github.com>
2022-12-08 16:56:02 +0000

    fix(notification-banner): Add support for call blocks
    This makes the notification banner component work correctly when a [call block](https://mozilla.github.io/nunjucks/templating.html#call) is used.

--- src/nunjucks/moduk/components/notification-banner/__tests__/macro.test.ts
+++ src/nunjucks/moduk/components/notification-banner/__tests__/macro.test.ts
@@ -1,9 +1,32 @@
 import { describe, expect, it } from 'vitest'
 
-import { renderFile } from '../../../../../test-utils'
+import { render, renderFile } from '../../../../../test-utils'
 
 describe('Notification banner', async () => {
-  it('renders the content', () => {
-    expect(() => renderFile('moduk/components/notification-banner/__examples__/default.njk')).not.toThrowError()
+  it('renders the content when using the text property', () => {
+    const element = renderFile('moduk/components/notification-banner/__examples__/default.njk')
+    expect(element).toHaveTextContent('There may be a delay')
+  })
+
+  it('renders the content when using the html property', () => {
+    const element = render(`
+{% from "moduk/components/notification-banner/macro.njk" import modukNotificationBanner %}
+
+{{ modukNotificationBanner({
+  html: "There may be a delay in processing your application because of the coronavirus outbreak."
+}) }}
+`)
+    expect(element).toHaveTextContent('There may be a delay')
+  })
+
+  it('renders the content when using a call block', () => {
+    const element = render(`
+{% from "moduk/components/notification-banner/macro.njk" import modukNotificationBanner %}
+
+{% call modukNotificationBanner({}) %}
+  There may be a delay<br> in processing your application because of the coronavirus outbreak.
+{% endcall %}
+`)
+    expect(element).toContainHTML('There may be a delay<br> in')
   })
 })
