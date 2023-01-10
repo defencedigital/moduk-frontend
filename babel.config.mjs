7d31acd4cf84d508652d2be3d6427f4ba4bb96cf
Adam L Edwards <adam@aeweb.io>
2023-01-10 10:22:32 +0000

    fix: IE11 build

---
+++ babel.config.mjs
@@ -0,0 +1,7 @@
+export default {
+  comments: false,
+  presets: [
+    ['@babel/preset-env', { modules: false }],
+    '@babel/preset-typescript',
+  ],
+}
---
+++ webpack.config.ts
@@ -0,0 +1,31 @@
+import path from 'node:path'
+import { Configuration } from 'webpack'
+
+const config: Configuration = {
+  context: path.resolve(__dirname),
+  devtool: 'source-map',
+  entry: './src/client/index.ts',
+  mode: 'production',
+  module: {
+    rules: [
+      {
+        test: /\.(m?js|ts)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader'],
+      },
+    ],
+  },
+  output: {
+    library: {
+      name: 'MODUK',
+      type: 'umd',
+    },
+    filename: 'moduk-frontend.umd.js',
+    path: path.resolve(__dirname, './dist/client'),
+  },
+  resolve: {
+    extensions: ['.ts', '.mjs', '...'],
+  },
+}
+
+export default config
