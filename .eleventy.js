6d13392cbc52b2eaedc7d2595ff2166bd3859b25
Adam L Edwards <5575331+adamledwards@users.noreply.github.com>
2023-05-11 09:49:58 +0100

    feat(react): Add react example pages

--- .eleventy.js
+++ .eleventy.js
@@ -3,12 +3,31 @@ require('ts-node').register({
   project: 'tsconfig.json',
 })
 
+const path = require('node:path')
+const { createElement, StrictMode } = require('react')
+const { renderToString } = require('react-dom/server')
+
 const { createNunjucksEnvironment } = require('./src')
 
 module.exports = (config) => {
   config.setLibrary('njk', createNunjucksEnvironment())
 
   config.addPassthroughCopy({ dist: '.' })
+
+  config.addWatchTarget(path.join(__dirname, 'src/react'))
+  config.addShortcode('react', async (componentPath) => {
+    /* The eslint rule import/no-dynamic-require is asking for
+    * await import() however this has been difficult to get this working the ts-node
+    * https://github.com/TypeStrong/ts-node/issues/1548
+    */
+    // eslint-disable-next-line import/no-dynamic-require, global-require
+    const component = require(path.join(__dirname, 'src/react', componentPath)).default
+    return renderToString(
+      createElement(StrictMode, {
+        children: createElement(component),
+      }),
+    )
+  })
 
   return {
     dir: {
