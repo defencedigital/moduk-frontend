7cd6a3cb8123e5779f43e6a8d085beafcc5a30c6
jpveooys <66470099+jpveooys@users.noreply.github.com>
2023-09-04 16:47:54 +0100

    feat(warning-text): Add React implementation
    This adds a React implementation of the warning text component.

---
+++ src/react/warning-text/WarningText.tsx
@@ -0,0 +1,35 @@
+import clsx from 'clsx'
+import { type ComponentPropsWithoutRef, forwardRef, type ReactNode } from 'react'
+
+export interface WarningTextProps extends ComponentPropsWithoutRef<'div'> {
+  /**
+   * Warning text content.
+   */
+  children: ReactNode
+  /**
+   * The fallback text for the icon. Defaults to ‘Warning’
+   */
+  iconFallbackText?: string
+}
+
+/**
+ * Warning text.
+ *
+ * @experimental React components are in alpha and subject to change.
+ *
+ * @example
+ * <WarningText>Completed</WarningText>
+ */
+export const WarningText = forwardRef<HTMLDivElement, WarningTextProps>(
+  ({ children, className, iconFallbackText = 'Warning', ...rest }, ref) => (
+    <div ref={ref} className={clsx('govuk-warning-text', className)} {...rest}>
+      <span className='govuk-warning-text__icon' aria-hidden='true'>!</span>
+      <strong className='govuk-warning-text__text'>
+        <span className='govuk-warning-text__assistive'>{iconFallbackText}</span>
+        {children}
+      </strong>
+    </div>
+  ),
+)
+
+WarningText.displayName = 'WarningText'
---
+++ src/react/warning-text/__examples__/default.tsx
@@ -0,0 +1,5 @@
+import { WarningText } from '@moduk/frontend/react'
+
+export const Example = () => (
+  <WarningText iconFallbackText='Warning'>You must appeal within 1 year of getting your decision letter.</WarningText>
+)
