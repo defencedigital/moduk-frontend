3aebb100ec5cfdf51f996308a2feb16c32004602
Angus McConnell <angus.mcconnell@accenture.com>
2025-02-03 15:05:39 +0000

    fixup! fix: Fix flaky tests in CI

--- e2e/tests/visual-regression.spec.ts
+++ e2e/tests/visual-regression.spec.ts
@@ -8,7 +8,7 @@ test.describe('@visual-regression', () => {
         test.describe(exampleName, () => {
           test.beforeEach(async ({ page }) => {
             await page.goto(`./components/${component}/${exampleName}`)
-            await page.waitForTimeout(300)
+            await page.waitForTimeout(350)
           })
 
           test('matches the saved screenshot', async ({ componentElement }) => {
