0dcf5fca139fe15d6ecc56d76f719b07bd9ccda1
Angus McConnell <angus.mcconnell@accenture.com>
2025-06-16 17:05:50 +0100

    test: Expect TS error
    There is a bug with TS/vitest where TS incorrectly expects an object to have type `Expected: undefined, Actual: string`

--- src/react/internal/Link/__tests__/Link.test-d.tsx
+++ src/react/internal/Link/__tests__/Link.test-d.tsx
@@ -13,13 +13,15 @@ export const LinkExtended: LinkComponent<{ active: boolean }> = forwardRef<unkno
 
 describe('Link', () => {
   test('LinkBase types', () => {
-    expectTypeOf(LinkBase<'a'>).parameter(0).toMatchTypeOf<
+    expectTypeOf(LinkBase<'a'>).parameter(0).toExtend<
       {
         component?: 'a'
         ref?: Ref<HTMLAnchorElement>
       }
     >()
-    expectTypeOf(LinkBase<typeof RouterLink>).parameter(0).toMatchTypeOf<
+    expectTypeOf(LinkBase<typeof RouterLink>).parameter(0).toExtend<
+      // @ts-expect-error There is a bug with TS/vitest here - getting this error message:
+      // Type 'Ref<RouterLink> | undefined' is not assignable to type '"Expected: undefined, Actual: string"
       {
         component?: typeof RouterLink
         ref?: Ref<RouterLink>
@@ -29,19 +31,21 @@ describe('Link', () => {
   })
 
   test('LinkComponent types', () => {
-    expectTypeOf<LinkComponent<{ active: boolean }>>().parameter(0).toMatchTypeOf<
+    expectTypeOf<LinkComponent<{ active: boolean }>>().parameter(0).toExtend<
       {
         active: boolean
       }
     >()
-    expectTypeOf(LinkExtended<'a'>).parameter(0).toMatchTypeOf<
+    expectTypeOf(LinkExtended<'a'>).parameter(0).toExtend<
       {
         component?: 'a'
         ref?: Ref<HTMLAnchorElement>
         active: boolean
       }
     >()
-    expectTypeOf(LinkExtended<typeof RouterLink>).parameter(0).toMatchTypeOf<
+    expectTypeOf(LinkExtended<typeof RouterLink>).parameter(0).toExtend<
+      // @ts-expect-error There is a bug with TS/vitest here - getting this error message:
+      // Type 'Ref<RouterLink> | undefined' is not assignable to type '"Expected: undefined, Actual: string"
       {
         component?: typeof RouterLink
         ref?: Ref<RouterLink>
